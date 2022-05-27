c13fe45ff28a9c5e1aee8c38f84acbca0c9b0111
Đỗ Xuân Thắng <doxuanthang@outlook.com>
2022-05-27 11:42:18 +0700

    Update tiktok.py

--- tiktok.py
+++ tiktok.py
@@ -1,6 +1,10 @@
 import subprocess
 from time import sleep
-from colorama import Fore, Back, Style
+try:
+    from colorama import Fore, Back, Style
+except:
+    subprocess.run("pip install colorama")
+    from colorama import Fore, Back, Style
 print(Fore.RED + 'some red text')
 print(Back.GREEN + 'and with a green background')
 print(Style.RESET_ALL)
