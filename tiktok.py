0482023d8bd0af4696c8df1a40c2c014d3a7a5df
Đỗ Xuân Thắng <doxuanthang@outlook.com>
2022-05-27 11:39:51 +0700

    Create tiktok.py

---
+++ tiktok.py
@@ -0,0 +1,7 @@
+import subprocess
+from time import sleep
+from colorama import Fore, Back, Style
+print(Fore.RED + 'some red text')
+print(Back.GREEN + 'and with a green background')
+print(Style.RESET_ALL)
+print('back to normal now')
